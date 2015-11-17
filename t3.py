881df3a35a95815d64c86b29ffbddaf66df2b482
girishramnani <girishramnani95@gmail.com>
2015-11-17 20:21:15 +0530

    initial commit

---
+++ t3.py
@@ -0,0 +1,22 @@
+import curses
+stdscr = curses.initscr()
+
+def curses_up():
+    """
+    all the initallization of the curses here
+
+    """
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+
+def curses_down():
+    """
+    function to get everything back to normal
+    """
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+
+curses_up()
+
