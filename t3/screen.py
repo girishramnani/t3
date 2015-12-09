8aea607063f2dacd5ea4d3f3166be2b17b480f05
girishramnani <girishramnani95@gmail.com>
2015-12-09 14:47:11 +0530

    added the location and wrap around

--- t3/screen.py
+++ t3/screen.py
@@ -1,7 +1,52 @@
+import curses
 
 
 class Screen(object):
-	
-	def __init__(self,buffer=None):
+	def __init__(self,buffer):
 		self.buffer = buffer
 		
+
+	
+	def __call__(self,stdscr):
+		"""working along the wrapper of the curses for better opening an closing of the program"""
+		self.window = stdscr
+	
+	
+	
+
+class Location(object):
+
+	def __init__(self,size,x=0,y=0):
+		self._x = x
+		self._y = y
+		self.size=size
+	
+	@property
+	def x(self):
+		return self._x%self.size[1]
+	
+	@property
+	def y(self):
+		return self._y%self.size[0]	
+	
+	@x.setter	
+	def x(self,value):
+		self._x = value%self.size[1]
+	
+	@y.setter
+	def y(self,value):
+		self._y = value%self.size[0]
+	
+
+class Buffer(object):
+	
+	"""
+	This object is responsible for the cursor movement and coloring of the code
+	
+	"""	
+	
+	def __init__(self,init_loc):
+		self.init_loc = init_loc
+				
+		
+
---
+++ tests/location_test.py
@@ -0,0 +1,25 @@
+import unittest
+from t3.screen import Location
+
+class Location_test(unittest.TestCase):
+	
+	
+	def setUp(self):
+		self.loc = Location((20,20))
+		
+	
+	def test_for_simple(self):
+		self.assertEqual(self.loc.x,0)
+		self.assertEqual(self.loc.y,0)
+	
+	def test_for_adding(self):
+		self.loc.x+=21
+		self.assertEqual(self.loc.x,1)
+		
+	def test_for_subtraction(self):
+		self.loc.y-=1
+		self.assertEqual(self.loc.y,19)
+		
+	
+if __name__ =="__main__":
+	unittest.main()
